c8176ba584fb5ad0f190992290147f81a156a0bf
Ionică Bizău <bizauionica@yahoo.com>
2014-11-09 11:44:30 +0200

    Added git handlers

---
+++ git-handlers.js
@@ -0,0 +1,14 @@
+var Git = require("git-tools");
+
+var GitHandlers = module.exports = {};
+
+var Repo = new Git(Bloggify.config.pathContent);
+GitHandlers.update = function (message, callback) {
+    Repo.exec("diff", function (err, data) {
+        if (err) { return callback(err); }
+        if (!data) {
+            return callback(null, null);
+        }
+        Repo.exec("commit", "-m", message, ".", callback);
+    });
+};
