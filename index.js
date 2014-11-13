15049005c87cfa72e158df615369acc7c759b6a2
Ionică Bizău <bizauionica@yahoo.com>
2014-11-13 21:13:57 +0200

    Namespaced lib files

--- index.js
+++ index.js
@@ -1,108 +1,63 @@
 // Dependencies
-var Api = require("./api");
+var Api = require("./api")
+  , ResponseHandler = require("./lib/response")
+  , SessionCheck = require("./lib/session_check")
+  ;
 
 // Bloggify APIs
 module.exports = function (api) {
-
-    /**
-     * responseHandler
-     * Generates a response handler function.
-     *
-     * @name responseHandler
-     * @function
-     * @param {Object} lien The lien object.
-     * @return {Function} The response handler.
-     */
-    function responseHandler(lien) {
-        return function (err, data) {
-            if (err) {
-                if (typeof err.statusCode !== "number") {
-                    Bloggify.log(err, "error");
-                    lien.end({
-                        error: "Internal server error."
-                    }, 500);
-                } else {
-                    lien.end(err, err.statusCode);
-                }
-                return;
-            }
-            lien.end(data);
-        }
-    }
-
-    /**
-     * sessionCheck
-     * Checks if the session exists.
-     *
-     * @name sessionCheck
-     * @function
-     * @param {Object} lien The lien object.
-     * @return {Boolean} `true` if session exists, otherwise `false`.
-     */
-    function sessionCheck(lien) {
-        var sessionData = lien.session.getData();
-        if (!sessionData) {
-            return lien.end({
-                error: "autorize_error"
-              , message: "You're not authorized."
-            }, 403);
-            return true;
-        }
-        return false;
-    }
-
     /*!
      *  Rest API
      * */
     // List articles
     Bloggify.server.page.add("/api/articles", function (lien) {
-        Api.articles.list.call(lien, lien.data, responseHandler(lien));
+        Api.articles.list.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // Get Article
     Bloggify.server.page.add(/\/api\/article\/[0-9]+$/, function (lien) {
         var id = (lien.pathName.match(/\/api\/article\/([0-9]+)$/) || [])[1];
-        Api.articles.get.call(lien, id, lien.data, responseHandler(lien));
+        Api.articles.get.call(lien, id, lien.data, ResponseHandler(lien));
     });
 
     // Save article
     Bloggify.server.page.add("/api/save/article", "post", function (lien) {
-        if (sessionCheck(lien)) { return; }
-        Api.articles.save.call(lien, lien.data, responseHandler(lien));
+        if (SessionCheck(lien)) { return; }
+        Api.articles.save.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // Delete article
     Bloggify.server.page.add("/api/delete/article", "post", function (lien) {
-        if (sessionCheck(lien)) { return; }
-        Api.articles.delete.call(lien, lien.data, responseHandler(lien));
+        if (SessionCheck(lien)) { return; }
+        Api.articles.delete.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // List pages
     Bloggify.server.page.add("/api/pages", function (lien) {
-        Api.pages.list.call(lien, lien.data, responseHandler(lien));
+        Api.pages.list.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // Get Page
     Bloggify.server.page.add(/\/api\/page\/.*/, function (lien) {
         // TODO Regex
         var slug = (lien.pathName.match(/\/api\/page\/(.*)/) || [])[1] || "";
-        Api.pages.get(slug, lien.data, responseHandler(lien));
+        Api.pages.get(slug, lien.data, ResponseHandler(lien));
     });
 
     // Save page
     Bloggify.server.page.add("/api/save/page", "post", function (lien) {
-        if (sessionCheck(lien)) { return; }
-        Api.pages.save.call(lien, lien.data, responseHandler(lien));
+        if (SessionCheck(lien)) { return; }
+        Api.pages.save.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // Delete page
     Bloggify.server.page.add("/api/delete/page", "post", function (lien) {
-        Api.pages.delete.call(lien, lien.data, responseHandler(lien));
+        Api.pages.delete.call(lien, lien.data, ResponseHandler(lien));
     });
 
     // Sync with remote
     Bloggify.server.page.add("/api/sync", function (lien) {
-        if (sessionCheck(lien)) { return; }
-        Api.util.sync.call(lien, responseHandler(lien));
+        if (SessionCheck(lien)) { return; }
+        Api.util.sync.call(lien, ResponseHandler(lien));
     });
 };
